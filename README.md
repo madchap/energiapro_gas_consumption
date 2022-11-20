ddf8078eda66a8fe11fedf6e74af4adffd378eb7
Fred Blaise <fred.blaise@protonmail.com>
2022-11-20 09:11:47 +0100

    move away from selenium

--- README.md
+++ README.md
@@ -3,20 +3,16 @@
 # EnergiaPro gas consumption readings
 This appdaemon will go fetch your gas data from EnergiaPro's customer portal, which you would find at https://www.holdigaz.ch/espace-client/views/view.login.php
 
-This appdaemon's app only supports 1 gas installation.
-
-> Warning: This code is based on Selenium and may break as the vendor is making changes to their website. If that's the case, please open up an issue. Otherwise, wait for an update, as for the foreseeable future, I will be using this appdaemon myself.
-
-This code was written on x64 docker containers and runs in "production" on a Raspberry Pi 3b. I am not a developer, use at your own risk!
+This appdaemon's app only supports 1 gas installation (at least for now).
 
 ## High-level flow
-- Login to customer portal and download xls file (using Selenium)
+- Login to customer portal and download xls file
 - Grab the excel file, and consume its last line
 - POSTs the data to the following two new HA sensors:
   - `sensor.energiapro_gas_daily` (m3 of gas consumed in the day)
   - `sensor.energiapro_gas_total` (actual "relevé" in m3 on your installation)
 
-There seems to be 2 postings a day to the portal. If you fetch the number somewhere in the middle of the afternoon, you'd have a certain number. If you read the next morning (for the previous day), the number will have been updated and the previous number overwritten.
+There seems to be 2 postings a day to the portal, but the numbers are usually available the day after. As far as I know, there is no "back-posting" the data in HA, unless we fiddle with the database, so the data will be skewed in time.
 
 # Energiapro pre-requisite
 - Your gas installation is already equipped with EnergiaPro's LoraWan equipement.
@@ -24,16 +20,17 @@ There seems to be 2 postings a day to the portal. If you fetch the number somewh
 
 If you are not equipped with the LoraWan stuff, you should be able to contact EnergiaPro and request its installation and configuration at no charge.
 
-## AppDaemon's python and system packages pre-requisites
+You will need to have the following information for configuration:
+- Your installation number, which you can find in the customer portal or on your invoice.
+  - As you configure this number for this app, the format looks like `123456.000`
+- Your client number, which you can find on your invoice
+  - The format is more like `123456`
+
+## AppDaemon's python packages pre-requisites
 Make sure you have the following python packages installed:
 - xlrd
 - pandas
 - requests
-- selenium
-
-You will need `chromium-chromedriver` system package installed as well for selenium.
-
-You may also have to have support for `xvfb` on your underlying O/S, if you are not running headless.
 
 ## Configuration
 ### secrets.yaml
@@ -43,6 +40,7 @@ You will need the following in your secrets.yaml file
 energiapro_email: <YOUR_EMAIL>
 energiapro_password: <YOUR_PASSWORD>
 energiapro_installation_number: "<YOUR_INSTALLATION_NUMBER>"
+energiapro_client_number: "<YOUR_CLIENT_NUMBER>"
 energiapro_bearer_token: <HA_LONG_LIVE_TOKEN>
 ```
 
@@ -61,11 +59,14 @@ energiapro_gas_consumption:
   energiapro_password: !secret energiapro_password
   energiapro_bearer_token: !secret energiapro_bearer_token
   energiapro_installation_number: !secret energiapro_installation_number
+  energiapro_client_number: !secret energiapro_client_number
   # ha_url: http://localhost:8123  # optional, in case hassplugin ha_url undefined
 ```
 
 ## Manually trigger the app
-The app registers an endpoint at `energiapro_gas_consumption`. If you want to trigger a run manually, you can call that endpoint. Example:
+The app can register an endpoint at `energiapro_gas_consumption`, which was mainly used during development. It's been commented for "production".
+
+If you want to trigger a run manually, uncomment the necessary line in the `initialize` method and you then can call that endpoint, such as:
 
 ```
 $ curl -XPOST -i -H "Content-Type: application/json"  http://<YOUR_APPDAEMON_IP>:<YOUR_APPDAEMON_PORT>/api/appdaemon/energiapro_gas_consumption -d '{"action": "Call of Duty"}'
@@ -74,14 +75,6 @@ $ curl -XPOST -i -H "Content-Type: application/json"  http://<YOUR_APPDAEMON_IP>
 # Troubleshhoting
 ## No error, but no data either
 - Make sure you've configured your installation number within double quotes and that it is the right number.
-
-## unknown error: net::ERR_NAME_NOT_RESOLVED (Session info: headless chrome=<VERSION>)
-That's a good question. If you have an answer, please let me know :-)
-Workaround: Install `xvfb` as system package and comment out the following around line 135:
-
-```
-chrome_options.add_argument("--headless")
-```
 
 # TODO:
 - how to backdate for previous day? (e.g. come up with good SQL probably)
--- apps/energiapro_gas_consumption/energiapro_gas.py
+++ apps/energiapro_gas_consumption/energiapro_gas.py
@@ -1,16 +1,8 @@
 import appdaemon.plugins.hass.hassapi as hassapi
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.chrome.options import Options
-import glob
-import os, sys
 from pathlib import Path
 import pandas as pd
 import tempfile
-import time
+from datetime import datetime
 import requests
 
 
@@ -28,21 +20,23 @@ class EnergiaproGasConsumption(hassapi.Hass):
 
     def initialize(self):
         # register an API endpoint for manual triggering
-        self.register_endpoint(self.my_callback, "energiapro_gas_consumption")
-        self.run_at_sunrise(self.get_gas_data)
+        # self.register_endpoint(self.my_callback, "energiapro_gas_consumption")
+        minutes = 60
+        self.log(f"Will fetch gas data every {minutes} minutes")
+        self.run_every(self.get_gas_data, datetime.now(), minutes * 60)
+        # self.run_at_sunrise(self.get_gas_data)
 
     def convert_xls_to_csv(self, xls_filename):
         xls_data = pd.read_excel(xls_filename, engine="xlrd")
+        csv_file = f"{download_folder}/energiapro_{self.args['energiapro_installation_number']}_data.csv"
+
         xls_data.to_csv(
-            f"{download_folder}/energiapro_{self.args['energiapro_installation_number']}_data.csv",
+            csv_file,
             index=None,
             header=True,
         )
-        df = pd.DataFrame(
-            pd.read_csv(
-                f"{download_folder}/energiapro_{self.args['energiapro_installation_number']}_data.csv"
-            )
-        )
+        df = pd.DataFrame(pd.read_csv(csv_file))
+
         if len(df.columns) == 0:
             self.log(
                 f"Didn't seem to get any data in the Excel file... count is {df.count()}."
@@ -51,40 +45,16 @@ class EnergiaproGasConsumption(hassapi.Hass):
 
         self.post_to_entities(df)
 
-    def get_downloaded_file_name(self):
-        files = glob.glob(
-            download_folder + f"/*{self.args['energiapro_installation_number']}*.xls"
-        )
-        last_file = max(files, key=os.path.getctime)
-
-        return last_file
-
     def cleanup_files(self):
         self.log("Cleaning up files")
         p = Path(download_folder)
-        files_to_remove = list(
-            # p.glob(f"*{self.args['energiapro_installation_number']}*.xls")
-            p.glob("*")
-        )
+        files_to_remove = list(p.glob("*"))
         for file in files_to_remove:
             file.unlink()
 
         p.rmdir()
 
     def post_to_entities(self, df):
-        try:
-            ha_url = self.config["plugins"]["HASS"]["ha_url"]
-            if self.args["ha_url"]:
-                # get HA's url from app's first, if configured/overriden by user
-                self.log("Using ha_url from app's configuration")
-                ha_url = self.args["ha_url"]
-        except Exception as e:
-            self.log(
-                "No Home Assistant URL could be found. Please configure ha_url in the app's configuration. Aborting."
-            )
-            self.log(e)
-            return
-
         def _post_daily_consumption():
             entity_url = f"{ha_url}/api/states/sensor.energiapro_gas_daily"
             self.log("POST'ing data to {}".format(entity_url))
@@ -101,7 +71,6 @@ class EnergiaproGasConsumption(hassapi.Hass):
                     "state_class": "total",
                 },
             }
-
             r = requests.post(entity_url, json=daily_payload, headers=headers)
             self.log("POST'ing status: {}".format(r.status_code))
 
@@ -121,9 +90,21 @@ class EnergiaproGasConsumption(hassapi.Hass):
                     "state_class": "total_increasing",
                 },
             }
-
             r = requests.post(entity_url, json=total_payload, headers=headers)
             self.log("POST'ing status: {}".format(r.status_code))
+
+        try:
+            ha_url = self.config["plugins"]["HASS"]["ha_url"]
+            if self.args["ha_url"]:
+                # get HA's url from app's first, if configured/overriden by user
+                self.log("Using ha_url from app's configuration")
+                ha_url = self.args["ha_url"]
+        except Exception as e:
+            self.log(
+                "No Home Assistant URL could be found. Please configure ha_url in the app's configuration. Aborting."
+            )
+            self.log(e)
+            return
 
         _post_daily_consumption()
         _post_total_consumption()
@@ -131,57 +112,44 @@ class EnergiaproGasConsumption(hassapi.Hass):
     def get_gas_data(self):
         base_url = self.args.get("energiapro_base_url")
         login_url = f"{base_url}/views/view.login.php"
-        csv_base_export_link = f"{base_url}/views/view.statistiques.lpn.php?a="
+        login_controller_link = f"{base_url}/controllers/controller.login.php"
+        export_controller_link = f"{base_url}/controllers/controller.export.xls.php"
 
         try:
-            chrome_options = webdriver.chrome.options.Options()
-            chrome_options.add_argument("--headless")
-            chrome_options.add_argument("--window-size=1024,768")
-            chrome_options.add_argument("--disable-dev-shm-usage")
-            chrome_options.add_argument("--disable-gpu")
-            chrome_options.add_argument("--no-sandbox")
-            if self.args.get("energiapro_no_ssl_check") == "true":
-                chrome_options.add_argument("--ignore-certificate-errors")
-            prefs = {"download.default_directory": download_folder}
-            chrome_options.add_experimental_option("prefs", prefs)
-            driver = webdriver.Chrome(options=chrome_options)
+            login_payload = {
+                "email": self.args["energiapro_email"],
+                "password": self.args["energiapro_password"],
+            }
 
-            driver.implicitly_wait(3)
-            self.log("Logging in")
-            driver.get(login_url)
-            email_el = driver.find_element(By.ID, "email")
-            passwd_el = driver.find_element(By.ID, "password")
-            email_el.clear()
-            email_el.send_keys(self.args["energiapro_email"])
-            passwd_el.clear()
-            passwd_el.send_keys(self.args["energiapro_password"])
-            driver.find_element(By.XPATH, '//button[text()="Login"]').click()
+            export_payload = {
+                "instNum": self.args["energiapro_installation_number"],
+                "adrAbo": self.args["energiapro_client_number"],
+            }
+        except Exception as config_e:
+            self.log("There was a problem getting configuration values. Aborting.")
+            return
 
-            # self.log("Waiting for portal to come up...")
-            elem = WebDriverWait(driver, 30).until(
-                EC.url_contains("view.espace-client.php")
-            )
+        try:
+            with requests.Session() as s:
+                s.get(login_url)
+                r = s.post(login_controller_link, data=login_payload)
+                local_filename = f"{download_folder}/energiapro_{self.args['energiapro_installation_number']}_data.xls"
 
-            # self.log("Navigate to consumption page")
-            # navigate to consumption page
-            driver.get(
-                f"{csv_base_export_link}{self.args['energiapro_installation_number']}"
-            )
+                if (
+                    r.status_code == 200 and r.text == "true"
+                ):  # The controller 'true' resp is the real thing
+                    self.log("Login successful")
 
-            # download file
-            # self.log("Identifying and waiting for download link")
-            export_link = driver.find_element(By.ID, "exportLPN")
-            # export_link = driver.find_element(By.LINK_TEXT, "Exporter tous les relevés")
-            elem = WebDriverWait(driver, 30).until(
-                EC.element_to_be_clickable(export_link)
-            )
-            self.log("Downloading xls file")
-            driver.find_element(By.ID, "exportLPN").click()
-            time.sleep(2)
-            filename = self.get_downloaded_file_name()
-            self.convert_xls_to_csv(filename)
-            self.cleanup_files()
+                    with s.post(
+                        export_controller_link, data=export_payload, stream=True
+                    ) as dl:
+                        dl.raise_for_status()
+                        with open(local_filename, "wb") as f:
+                            for c in dl.iter_content(chunk_size=8192):
+                                f.write(c)
+                    self.log("File downloaded")
+
+                self.convert_xls_to_csv(local_filename)
+                self.cleanup_files()
         except Exception as e:
             self.log(e)
-        finally:
-            driver.quit()
